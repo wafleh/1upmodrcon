e7729f80f0965a7f7a89ccc1c8f9ac45a063e5e1
theletterpi <theletterpi@a7af1024-e983-f20c-8043-f614c9338961>
2010-09-09 04:52:30 +0000

    Work on About window, fixed authors in BowserQuery.

--- trunk/1upModRcon/src/modrcon/AboutWindow.java
+++ trunk/1upModRcon/src/modrcon/AboutWindow.java
@@ -1,29 +1,53 @@
 package modrcon;
 
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
+ * Displays an About window for 1up ModRcon.
  *
  * @author Pyrite
  */
-public class AboutWindow extends JDialog {
+public class AboutWindow extends JDialog implements ActionListener {
 
     private MainWindow parent;
+
+    private JTabbedPane tabbedPane;
 
     public AboutWindow(MainWindow owner) {
         super();
         this.parent = owner;
         this.setTitle("1up ModRcon - About");
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setModal(true);
+        this.setSize(385, 310);
+        this.setResizable(false);
+        ImageIcon topLeftIcon = new ImageIcon(getClass().getResource("/modrcon/resources/1up8bit_green.png"));
+        this.setIconImage(topLeftIcon.getImage());
 
         // Setup the Content Pane
         Container cp = this.getContentPane();
-        cp.setLayout(new BorderLayout());
+        cp.setLayout(new BorderLayout());  
+
+        tabbedPane = new JTabbedPane();
+        JComponent panel1 = getAboutTab();
+        tabbedPane.addTab("About", panel1);
+        JComponent panel2 = getThirdPartiesTab();
+        tabbedPane.addTab("Third Parties", panel2);
+
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton btnClose = new JButton("Close");
+        btnClose.addActionListener(this);
+        buttonPanel.add(btnClose);
 
         cp.add(new LogoPanel(1), BorderLayout.NORTH);
+        cp.add(tabbedPane, BorderLayout.CENTER);
+        cp.add(buttonPanel, BorderLayout.SOUTH);
 
-        this.pack();
+        //this.pack();
 
         // Set the location of the About Window centered relative to the MainMenu
         // --CENTER--
@@ -37,6 +61,33 @@ public class AboutWindow extends JDialog {
         // --END CENTER--
 
         this.setVisible(true);
+    }
+
+    protected JComponent getThirdPartiesTab() {
+        JPanel panel = new JPanel(false);
+        panel.setLayout(new VerticalFlowLayout());
+        JLabel text1 = new JLabel("<html>Portions of this software use components<br>developed by the following 3rd Parties.</html>");
+        JLabel text2 = new JLabel("Q3Tool by Philip Edelbrock");
+        JLabel text3 = new JLabel("VerticalFlowLayout by Vassili Dzuba");
+        JLabel text4 = new JLabel("BrowserLauncher by Eric Albert");
+        panel.add(text1);
+        panel.add(text2);
+        panel.add(text3);
+        panel.add(text4);
+        return panel;
+    }
+
+    protected JComponent getAboutTab() {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel("About 1up ModRcon X");
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        this.dispose();
     }
 
 
