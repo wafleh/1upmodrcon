068157eac3797b671aacd90d016d64ce9188000a
lordizuriel <lordizuriel@a7af1024-e983-f20c-8043-f614c9338961>
2010-09-12 10:42:38 +0000

    Fixed a minor error I made in storing the property, I had original stored it with the default being false, when it should have been true.

--- trunk/1upModRcon/src/modrcon/PropertyManager.java
+++ trunk/1upModRcon/src/modrcon/PropertyManager.java
@@ -47,8 +47,8 @@ public class PropertyManager {
     public boolean getShowVoteWarning() {
         String temp = propFile.getProperty("votewarning");
         if (temp == null || temp.equals("")) {
-            propFile.setProperty("votewarning", "false");
-            return false;
+            propFile.setProperty("votewarning", "true");
+            return true;
         }
 
         return Boolean.parseBoolean(temp);
@@ -119,7 +119,7 @@ public class PropertyManager {
     private boolean writeDefaultPropertiesFile(File file) {
         Writer writer = null;
         try {
-            String text = "#Property File for 1up ModRcon\n#Fri Aug 27 23:18:57 CDT 2010\nversion=1.0\ngamepath=\nconsolebgcolor=\\#0\nconsolefgcolor=\\#FFFFFF\nreceivetimeout=100\nstatusonconnect=false\nvotewarning=false\n";
+            String text = "#Property File for 1up ModRcon\n#Fri Aug 27 23:18:57 CDT 2010\nversion=1.0\ngamepath=\nconsolebgcolor=\\#0\nconsolefgcolor=\\#FFFFFF\nreceivetimeout=100\nstatusonconnect=false\nvotewarning=true\n";
             writer = new BufferedWriter(new FileWriter(file));
             writer.write(text);
             return true;
