2a95ef16635f0305d96678d7f10b65b2fc725c4f
theletterpi <theletterpi@a7af1024-e983-f20c-8043-f614c9338961>
2010-09-20 09:00:09 +0000

    For importing 1up servers, the password dialog now uses a JPasswordField.

--- 1upModRcon/src/modrcon/ModRconUtil.java
+++ 1upModRcon/src/modrcon/ModRconUtil.java
@@ -1,16 +1,8 @@
 package modrcon;
 
-import java.awt.Cursor;
-import java.awt.Image;
-import java.awt.Point;
-import java.awt.Toolkit;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
