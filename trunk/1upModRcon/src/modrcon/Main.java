709dfa63fef93478762bf552dbfb1aa663b1ff24
theletterpi <theletterpi@a7af1024-e983-f20c-8043-f614c9338961>
2010-09-20 06:27:40 +0000

    Supports changing the Look and Feel from the Settings dialog.

--- trunk/1upModRcon/src/modrcon/Main.java
+++ trunk/1upModRcon/src/modrcon/Main.java
@@ -2,6 +2,7 @@ package modrcon;
 
 import javax.swing.*;
 import java.io.File;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 /**
  * The Main Class of 1up ModRcon.
@@ -14,18 +15,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Load Settings
+        final PropertyManager pm = new PropertyManager();
+        String lafName = pm.getLookAndFeel();
+        String lafClass = "";
+
+        if (lafName == null) {
+            lafClass = UIManager.getSystemLookAndFeelClassName();
+        }
+        else {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if (info.getName().equals(lafName)) {
+                    lafClass = info.getClassName();
+                }
+            }
+            if (lafClass.equals("")) {
+                lafClass = UIManager.getSystemLookAndFeelClassName();
+            }
+        }
         JFrame.setDefaultLookAndFeelDecorated(true);
         try {
             // Set the OS's Native Look and Feel
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel(lafClass);
         }
         catch (Exception e) {
-            System.out.println("Error: Was Unable to Set the OS's Native Widget Theme");
+            System.out.println("Error: Was Unable to Set the Look and Feel");
             System.out.println(e.getMessage());
-        }
-        
-        // Load Settings
-        final PropertyManager pm = new PropertyManager();
+        }     
 
         // First see if servers.xml exists, if not, create it, and run ServerSetupWizard.
         File f = new File("servers.db");
