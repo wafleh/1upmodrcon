a3a875d5038e12f8f81d29306878c01a0c823122
jmvrbanac@gmail.com <jmvrbanac@gmail.com@a7af1024-e983-f20c-8043-f614c9338961>
2011-03-07 04:26:52 +0000

    Modified Linux version to cut off the skin when effects are disabled.

--- trunk/1upServerMonitor/PackageForUbuntu.sh
+++ trunk/1upServerMonitor/PackageForUbuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 FLEXSDKBIN="/home/john/FlexSDK/bin"
 AIRSDKBIN="/home/john/AirSDK/bin"
 CERTIFICATE="1upServerMonitor.pfx"
@@ -9,13 +9,27 @@ FILE_OR_DIR="-C bin ."
 PROJPATH="/home/john/Development/1upmodrcon/1upServerMonitor"
 
 # Compile
-#$FLEXSDKBIN/mxmlc -load-config+=obj/1upServerMonitorConfig.xml -debug=true -static-link-runtime-shared-libraries=true +configname=air -o $PROJPATH/bin/1upServerMonitor.swf
+if [ "$1" == "compile" ]
+then
+$FLEXSDKBIN/mxmlc -load-config+=obj/linux1upServerMonitorConfig.xml -debug=true -static-link-runtime-shared-libraries=true +configname=air -o $PROJPATH/bin/1upServerMonitor.swf
+fi
 
 # Package Air File
-#$FLEXSDKBIN/adt -package $SIGNING_OPTIONS $AIR_FILE $APP_XML $FILE_OR_DIR
+if [ "$1" == "air" ]
+then
+$FLEXSDKBIN/adt -package $SIGNING_OPTIONS $AIR_FILE $APP_XML $FILE_OR_DIR
+fi
 
 # Package for Debian
+if [ "$1" == "package" ]
+then
 $AIRSDKBIN/adt -package -target native natives/1upServerMonitor.deb air/1upServerMonitor.air
+fi
 
 # Debug
-#$AIRSDKBIN/adl application.xml bin
+if [ "$1" == "debug" ]
+then
+$AIRSDKBIN/adl application.xml bin
+fi
+
+echo "Finished..."
