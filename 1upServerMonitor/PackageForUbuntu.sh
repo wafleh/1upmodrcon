df81122dac32e884d65146c075f783b40b84fb4b
jmvrbanac <jmvrbanac@gmail.com>
2011-03-07 03:22:40 +0000

    Adding Ubuntu build files

---
+++ 1upServerMonitor/PackageForUbuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+FLEXSDKBIN="/home/john/FlexSDK/bin"
+AIRSDKBIN="/home/john/AirSDK/bin"
+CERTIFICATE="1upServerMonitor.pfx"
+SIGNING_OPTIONS="-storetype pkcs12 -keystore $CERTIFICATE -tsa none"
+AIR_FILE="air/1upServerMonitor.air"
+APP_XML="application.xml"
+FILE_OR_DIR="-C bin ."
+PROJPATH="/home/john/Development/1upmodrcon/1upServerMonitor"
+
+# Compile
+#$FLEXSDKBIN/mxmlc -load-config+=obj/1upServerMonitorConfig.xml -debug=true -static-link-runtime-shared-libraries=true +configname=air -o $PROJPATH/bin/1upServerMonitor.swf
+
+# Package Air File
+#$FLEXSDKBIN/adt -package $SIGNING_OPTIONS $AIR_FILE $APP_XML $FILE_OR_DIR
+
+# Package for Debian
+$AIRSDKBIN/adt -package -target native natives/1upServerMonitor.deb air/1upServerMonitor.air
+
+# Debug
+#$AIRSDKBIN/adl application.xml bin
