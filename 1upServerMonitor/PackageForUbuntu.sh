71aef066c2860a48b2ce3e2b40e1ca014887558d
jmvrbanac <jmvrbanac@gmail.com>
2011-03-15 03:11:29 +0000

    Packaging Ubuntu deb file and updating script

--- 1upServerMonitor/PackageForUbuntu.sh
+++ 1upServerMonitor/PackageForUbuntu.sh
@@ -12,24 +12,22 @@ PROJPATH="/home/john/Development/1upmodrcon/1upServerMonitor"
 if [ "$1" == "compile" ]
 then
 $FLEXSDKBIN/mxmlc -load-config+=obj/linux1upServerMonitorConfig.xml -debug=true -static-link-runtime-shared-libraries=true +configname=air -o $PROJPATH/bin/1upServerMonitor.swf
-fi
 
 # Package Air File
-if [ "$1" == "air" ]
+elif  [ "$1" == "air" ]
 then
 $FLEXSDKBIN/adt -package $SIGNING_OPTIONS $AIR_FILE $APP_XML $FILE_OR_DIR
-fi
 
 # Package for Debian
-if [ "$1" == "package" ]
+elif  [ "$1" == "package" ]
 then
 $AIRSDKBIN/adt -package -target native natives/1upServerMonitor.deb air/1upServerMonitor.air
-fi
 
 # Debug
-if [ "$1" == "debug" ]
+elif  [ "$1" == "debug" ]
 then
 $AIRSDKBIN/adl application.xml bin
-fi
 
-echo "Finished..."
+else
+echo "Please specify a argument [compile, air, package, debug]"
+fi
